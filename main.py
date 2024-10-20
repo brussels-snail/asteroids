1bc7aa4b9671b09ecf392aaadac9651e0c56f121
Brussels_Snail <itisvasilii@gmail.com>
2024-10-20 15:55:22 +0200

    black scren

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame, constants
+import pygame
+from constants import *
+
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+
+
 
 if __name__ == "__main__":
     main()
-
-print(f"Screen width: {constants.SCREEN_WIDTH}")
-print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    
